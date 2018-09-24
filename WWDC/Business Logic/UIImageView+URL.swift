f8a55f02452f6fcf003fcb4cea7f286594456cfd
thanhturin <thanhturin@gmail.com>
2018-09-24 22:49:59 +0800

    Set images

--- WWDC/Business Logic/UIImageView+URL.swift
+++ WWDC/Business Logic/UIImageView+URL.swift
@@ -5,10 +5,10 @@ extension UIImageView {
 
   func setImage(with url: URL?) {
     if let url = url {
-      af_setImage(withURL: url, placeholderImage: #imageLiteral(resourceName: "video_placeholder"))
+      af_setImage(withURL: url, placeholderImage: UIImage(named: "video_placeholder"))
     }
     else {
-      image = #imageLiteral(resourceName: "video_placeholder")
+      image = UIImage(named: "video_placeholder")
     }
   }
 
